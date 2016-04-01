8606d13aa5a510eb4674f38b74292576e898bb98
Dênnis Dantas <ddspog@gmail.com>
2016-04-01 18:39:42 -0300

    Add JS import

---
+++ app.js
@@ -0,0 +1,6 @@
+// Angular JS and Needed Libraries
+$.getScript( "//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js" );
+$.getScript( "//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-sanitize.js" );
+$.getScript( "//angular-ui.github.io/ui-router/release/angular-ui-router.js" );
+// Bootstrap JS
+$.getScript( "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" );
