f25309019b67b34773e9c576a2f85be02a886443
Dênnis Dantas <ddspog@gmail.com>
2016-04-02 13:08:33 -0300

    New Section

--- app.js
+++ app.js
@@ -68,7 +68,12 @@ homeApp.controller('HomeController', function($scope) {
 		social: [
 			{ text: 'Login via Facebook',
 			  icon: 'facebook-official',
-			  url: '/login' }
+			  url: '/login' },
+			{
+			  text: 'Ver anúncios',
+			  icon: 'birthday-cake',
+			  url: '/dashboard'
+			}
 		],
 		cellphoneimg: 'https://lh3.googleusercontent.com/wgSvsQJ2SFp4R2MbxNMyK-XtLZ6ONomliNQUH4SmWPLpVeOhGi7wWA8QL3RfhT3SckG35LIMgNR3GjT5DjRZxnQn86GAnYboBCSRKXToIDCQXlT_fFjhSTPPQS2wfFmI-sazLlgR7Owf3N0jghb-s5LhInpX_Ne9vGKXlUYEpk4wARDb7xCiwX-W6Qw33J9yKbs051ZmOvhxAlCuu_81ujdxu8NDz_fuLOh0dng65mPD0cofdlzkBKcPO0A5kB1OzaF7k4HoX0GhlbjScN0J5nJSZGP3-5RirwoY3FPg_c3Ft6Ei_NHJ_HKNdh4N4KMRiwyMqMBv9sLMfo_N5IE6sPTWuXBInft5CVyRidtzq_3NIbA5rjPnGioA13z6-coF25HNGOmNCwYduM0-bPRRikY4BOLLrTTkFd_1gAY2tiBflCMGNJmqy_06RTRyVjVNrOg_Ot56pabQ5W06K16MsoiV51QLwoPGQChVyIOJAsMIxLeNaKCOzNB-ITvtWMchEPywoc_g8u-vpFr2M1pYjWNlcox4WfksY27jLmh_pT1pa3hpAfIyr8vIP7PaPjSoxmlA2A=s570-no'
 	};
@@ -86,4 +91,8 @@ homeApp.config(["$stateProvider", "$urlRouterProvider", function($stateProvider,
             url: "/",
             templateUrl: 'sections/initial.html'
         })
+		.state('about', {
+			url: "/",
+			templateUrl: 'sections/about.html'
+		})
 }]);
