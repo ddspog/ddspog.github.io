0e28a059cb982a995901fbf47bb79e5363fd7887
Dênnis Dantas <ddspog@gmail.com>
2016-04-02 03:39:20 -0300

    Add sections

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ homeApp.directive('footBar', function(){
     };
 });
 
+homeApp.directive('header', function(){
+    return {
+        restrict: 'E',
+        scope: {
+            item: '=item'
+        },
+        templateUrl: 'directives/header.html'
+    };
+});
+
 homeApp.controller('HomeController', function($scope) {
     $scope.navbarText = {
 		brand: {
@@ -52,7 +62,16 @@ homeApp.controller('HomeController', function($scope) {
 			'Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
 		]
 	};
-    $scope.headerText = {};
+    $scope.headerText = {
+		maintitle: 'O bolo dos seus sonhos.',
+		subtitle: 'Descubra confeiteiros em sua cidade. Faça encomendas faltando poucos dias para a festa. Compare preços. Avalie. Escolha os seus favoritos. Converse.',
+		social: [
+			{ text: 'Login via Facebook',
+			  icon: 'facebook-official',
+			  url: '/login' }
+		],
+		cellphoneimg: 'https://lh3.googleusercontent.com/wgSvsQJ2SFp4R2MbxNMyK-XtLZ6ONomliNQUH4SmWPLpVeOhGi7wWA8QL3RfhT3SckG35LIMgNR3GjT5DjRZxnQn86GAnYboBCSRKXToIDCQXlT_fFjhSTPPQS2wfFmI-sazLlgR7Owf3N0jghb-s5LhInpX_Ne9vGKXlUYEpk4wARDb7xCiwX-W6Qw33J9yKbs051ZmOvhxAlCuu_81ujdxu8NDz_fuLOh0dng65mPD0cofdlzkBKcPO0A5kB1OzaF7k4HoX0GhlbjScN0J5nJSZGP3-5RirwoY3FPg_c3Ft6Ei_NHJ_HKNdh4N4KMRiwyMqMBv9sLMfo_N5IE6sPTWuXBInft5CVyRidtzq_3NIbA5rjPnGioA13z6-coF25HNGOmNCwYduM0-bPRRikY4BOLLrTTkFd_1gAY2tiBflCMGNJmqy_06RTRyVjVNrOg_Ot56pabQ5W06K16MsoiV51QLwoPGQChVyIOJAsMIxLeNaKCOzNB-ITvtWMchEPywoc_g8u-vpFr2M1pYjWNlcox4WfksY27jLmh_pT1pa3hpAfIyr8vIP7PaPjSoxmlA2A=s570-no'
+	};
     $scope.bannerText = {};
     $scope.content = {};
 });
@@ -65,6 +84,6 @@ homeApp.config(["$stateProvider", "$urlRouterProvider", function($stateProvider,
     $stateProvider
         .state('initial', {
             url: "/",
-            template: ''
+            templateUrl: 'sections/initial.html'
         })
 }]);
