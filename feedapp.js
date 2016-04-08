78b18ebaa601db3bfa204cf22a0be85afaa98033
Dênnis Dantas <ddspog@gmail.com>
2016-04-08 14:55:27 -0300

    'dsd'

--- feedapp.js
+++ feedapp.js
@@ -78,37 +78,39 @@ feedApp.controller('AdsController', function($scope) {
 			title: 'Red Velvet Cake',
 			chef: 'Panificadora Severo',
 			imglink: 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2004/1/23/1/ss1d26_red_velvet_cake.jpg.rend.sniipadlarge.jpeg',
-			scale: 'lg-6',
 			price: '60,00'
 		},
 		{
 			title: 'Banana Split Cheesecake',
 			chef: 'Doces Maria Amor',
 			imglink: 'http://www.shugarysweets.com/wp-content/uploads/2014/04/banana-split-cheesecake-1.jpg',
-			scale: 'lg-3',
 			price: '40,00'
 		},
 		{
 			title: 'Caramel Apple Cheesecake',
 			chef: 'Panificadora Severo',
 			imglink: 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2007/4/12/0/bt0206_applecheesecake.jpg.rend.sniipadlarge.jpeg',
-			scale: 'lg-3',
 			price: '20,00'
 		},	
 		{
 			title: 'Torta Alemã',
 			chef: 'Panificadora Severo',
 			imglink: 'http://receitatodahora.com.br/wp-content/uploads/2014/09/Torta-alem%C3%A3.jpg',
-			scale: 'lg-5',
 			price: '50,00'
 		},
 		{
 			title: 'Red Velvet Cake',
 			chef: 'Bolos Doce Paixão',
-			imglink: 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2004/1/23/1/ss1d26_red_velvet_cake.jpg.rend.sniipadlarge.jpeg',
-			scale: 'lg-7',
+			imglink: 'https://s3.amazonaws.com/twduncan/recipe/1778/hero-cherry-red-velvet-cake.jpg',
 			price: '30,00'
+		},
+		{
+			title: 'Bolo de Rapadura',
+			chef: 'Bolos Doce Paixão',
+			imglink: 'http://mdemulher.abril.com.br/sites/mdemulher/files/styles/retangular_horizontal_2/public/migracao/receita-bolo-rapadura.jpg?itok=6C_EiXVJ',
+			price: '40,00'
 		}
+		
 	];
 });
 
