fbba823241ad0df866996961a6d5b8d8cde9292e
Dênnis Dantas <ddspog@gmail.com>
2016-04-14 12:22:47 -0300

    'das'

--- feedapp.js
+++ feedapp.js
@@ -77,7 +77,7 @@ feedApp.directive('adCard', function() {
 });
 
 // Controller that works on all Homepage
-feedApp.controller('FeedController', function($scope, $state) {
+feedApp.controller('FeedController', ["$scope", "$state", function($scope, $state) {
 	$scope.setStateTitle = function(){
 	  try{
 		  $scope.title = $state.current.data.title;
@@ -120,7 +120,7 @@ feedApp.controller('FeedController', function($scope, $state) {
 			'Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
 		]
 	};
-});
+}]);
 
 // Controller that works only on Initial section
 feedApp.controller('AdsController', function($scope) {
