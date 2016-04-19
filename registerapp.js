461dbc99a2bdc02d8ae99c133afb687c55604a19
Dênnis Dantas <ddspog@gmail.com>
2016-04-19 14:10:13 -0300

    'ds'

--- registerapp.js
+++ registerapp.js
@@ -24,7 +24,7 @@ homeApp.controller('RegisterController', function($scope, $timeout) {
 					$("#tab3").delay(500).addClass("is-active");
 					break;
 			}	
-		}, 1000);
+		}, 2000);
 	}
 	$scope.tab(1);
 });
