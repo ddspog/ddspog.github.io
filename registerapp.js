d8e8f92f42dc8f1d96991ff188468700de8b8216
Dênnis Dantas <ddspog@gmail.com>
2016-04-19 14:09:27 -0300

    'ds'

--- registerapp.js
+++ registerapp.js
@@ -1,28 +1,30 @@
 // Declare HomepageApp (used on html tag of index.html)
 var homeApp = angular.module('RegisterApp', ['ui.router', 'ngSanitize']);
 
-homeApp.controller('RegisterController', function($scope) {
+homeApp.controller('RegisterController', function($scope, $timeout) {
 	$scope.tab = function(index){
-		switch (index){
-			case 1:
-				$(".mdl-tabs__tab-bar .mdl-tabs__tab").each( function(){
-					$(this).removeClass("is-active");
-				});
-				$("#tab1").delay(500).addClass("is-active");
-				break;
-			case 2:
-				$(".mdl-tabs__tab-bar .mdl-tabs__tab").each( function(){
-					$(this).removeClass("is-active");
-				});
-				$("#tab2").delay(500).addClass("is-active");
-				break;
-			default:
-				$(".mdl-tabs__tab-bar .mdl-tabs__tab").each( function(){
-					$(this).removeClass("is-active");
-				});
-				$("#tab3").delay(500).addClass("is-active");
-				break;
-		}
+		$timeout(function() {
+			switch (index){
+				case 1:
+					$(".mdl-tabs__tab-bar .mdl-tabs__tab").each( function(){
+						$(this).removeClass("is-active");
+					});
+					$("#tab1").delay(500).addClass("is-active");
+					break;
+				case 2:
+					$(".mdl-tabs__tab-bar .mdl-tabs__tab").each( function(){
+						$(this).removeClass("is-active");
+					});
+					$("#tab2").delay(500).addClass("is-active");
+					break;
+				default:
+					$(".mdl-tabs__tab-bar .mdl-tabs__tab").each( function(){
+						$(this).removeClass("is-active");
+					});
+					$("#tab3").delay(500).addClass("is-active");
+					break;
+			}	
+		}, 1000);
 	}
 	$scope.tab(1);
 });
