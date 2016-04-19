d2d83e7b81003753ac59a234495e1f970993511c
Dênnis Dantas <ddspog@gmail.com>
2016-04-19 01:02:11 -0300

    'ds'

--- registerapp.js
+++ registerapp.js
@@ -2,23 +2,4 @@
 var homeApp = angular.module('RegisterApp', ['ui.router', 'ngSanitize']);
 
 homeApp.controller('RegisterController', function($scope) {
-    $scope.isTab01 = true;
-	$scope.isTab02 = false;
-	$scope.isTab03 = false;
-	$scope.tab01 = function(){
-		$scope.isTab01 = true;
-		$scope.isTab02 = false;
-		$scope.isTab03 = false;
-	};
-	$scope.tab02 = function(){
-		$scope.isTab01 = false;
-		$scope.isTab02 = true;
-		$scope.isTab03 = false;
-	};
-	$scope.tab03 = function(){
-		$scope.isTab01 = false;
-		$scope.isTab02 = false;
-		$scope.isTab03 = true;
-	};
-	
 });
