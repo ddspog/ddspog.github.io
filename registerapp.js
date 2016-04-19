565ff7fe305e82d49122d8fdcdafb0717b187d1d
Dênnis Dantas <ddspog@gmail.com>
2016-04-19 13:43:52 -0300

    'ds'

--- registerapp.js
+++ registerapp.js
@@ -2,25 +2,19 @@
 var homeApp = angular.module('RegisterApp', ['ui.router', 'ngSanitize']);
 
 homeApp.controller('RegisterController', function($scope) {
-	$scope.tab1 = false;
-	$scope.tab2 = false;
-	$scope.tab3 = false;
 	$scope.tab = function(index){
 		switch (index){
 			case 1:
-				$scope.tab1 = true;
-				$scope.tab2 = false;
-				$scope.tab3 = false;
+				$(".mdl-tabs__tab-bar").removeClass("is-active");
+				$("#tab1").toggleClass("is-active");
 				break;
 			case 2:
-				$scope.tab1 = false;
-				$scope.tab2 = true;
-				$scope.tab3 = false;
+				$(".mdl-tabs__tab-bar").removeClass("is-active");
+				$("#tab2").toggleClass("is-active");
 				break;
 			default:
-				$scope.tab1 = false;
-				$scope.tab2 = false;
-				$scope.tab3 = true;
+				$(".mdl-tabs__tab-bar").removeClass("is-active");
+				$("#tab3").toggleClass("is-active");
 				break;
 		}
 	}
