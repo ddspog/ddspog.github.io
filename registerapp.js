c5e13920a500738f190cdd1e1b40d02075e11897
Dênnis Dantas <ddspog@gmail.com>
2016-04-19 01:27:34 -0300

    'ds'

--- registerapp.js
+++ registerapp.js
@@ -4,3 +4,5 @@ var homeApp = angular.module('RegisterApp', ['ui.router', 'ngSanitize']);
 homeApp.controller('RegisterController', function($scope) {
 	$scope.tab = 1;
 });
+
+$(".mdl-tabs__tab-bar.mdl-tabs__tab").after();
