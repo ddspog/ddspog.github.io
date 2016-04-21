4c00c13f87819a552edc6b24fc3c38e069eba520
Dênnis Dantas <ddspog@gmail.com>
2016-04-21 03:54:23 -0300

    'ds'

--- registerapp.js
+++ registerapp.js
@@ -18,20 +18,18 @@ homeApp.controller('RegisterController', function($scope, $timeout) {
 					$("#tab2").delay(500).addClass("is-active");
 					break;
 				default:
-					$(".site-tabs__tab-bar .site-tabs__tab").each( function(){
-						$(this).removeClass("is-active");
-					});
-					$("#tab3").delay(500).addClass("is-active");
+					if($scope.terms_accepted){
+						$scope.terms_next_pressed = false;
+						$(".site-tabs__tab-bar .site-tabs__tab").each( function(){
+							$(this).removeClass("is-active");
+						});
+						$("#tab3").delay(500).addClass("is-active");
+					} else {
+						$scope.terms_next_pressed = true;
+					}
 					break;
 			}	
 		}, 1);
 	}
 	$scope.tab(1);
-	
-	$('#terms_next').restrain(['click'], [{
-        name: 'No animation should be running',
-        pre: function(el, ev){
-			return $scope.terms_accepted;
-		}
-    }]).reconfigureEvents();
 });
